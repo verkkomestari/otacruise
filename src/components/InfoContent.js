b1a46e81fdec6b6dce4c23a4d53350ce2746fb26
Veera Ihalainen <Veera.helli@gmail.com>
2022-08-31 15:03:45 +0300

    info content styling

--- src/components/InfoContent.js
+++ src/components/InfoContent.js
@@ -2,10 +2,10 @@
 
 const InfoContent = ({ info }) => {
   return (
-    <div className='page-content col-md-8'>
+    <div className='page-content col-md-9'>
       <h1 className='montserrat info-title'>{info.title}</h1>
       {info.content.map((part) => (
-        <div className='page-body'>
+        <div className='page-body' key={part.content}>
           <h2 className='montserrat subtitle'>{part.subtitle}</h2>
           <p>{part.body}</p>
         </div>
