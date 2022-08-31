d8ba4be8c8d5fc88de282a64c414c6c074dab00a
Veera Ihalainen <Veera.helli@gmail.com>
2022-08-31 15:04:48 +0300

    small info page edit

--- src/components/InfoContent.js
+++ src/components/InfoContent.js
@@ -2,7 +2,7 @@
 
 const InfoContent = ({ info }) => {
   return (
-    <div className='page-content col-md-9'>
+    <div className='page-content col-md-8'>
       <h1 className='montserrat info-title'>{info.title}</h1>
       {info.content.map((part) => (
         <div className='page-body' key={part.content}>
