55f98c5bc29a70b0933f75e2b228e63daa5c8eb6
Veera <veera.helli@gmail.com>
2022-09-21 16:11:33 +0300

    link to kide.app

--- src/components/InfoContent.js
+++ src/components/InfoContent.js
@@ -1,9 +1,23 @@
 // The text content on Info page
-
+import { Link } from 'react-router-dom'
 const InfoContent = ({ infoList }) => {
   return (
     <div className='page-content col-md-8'>
       <h1 className='montserrat info-title'>{infoList.title}</h1>
+      {infoList.title == 'Tickets' ? (
+        <div className='mb-4'>
+          <div className='btn callToAction' id='linkbtn'>
+            <a
+              href='https://kide.app/events/c153f086-a944-4782-a70a-a608a49be183'
+              target='_blank'>
+              {' '}
+              Go to Kide.app!
+            </a>
+          </div>
+        </div>
+      ) : (
+        <></>
+      )}
       {infoList.content.map((part) => (
         <div className='page-body' key={part.content}>
           <h2
