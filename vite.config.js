c5f718a59836606e6b2c84e2c642229ab5fb4e32
Eemeli Oksanen <eemelioksanen@protonmail.com>
2026-08-18 22:05:59 +0300

    add image optimizer

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    ViteImageOptimizer({
+      apply: 'serve',
+      png: {
+        quality: 80,
+      },
+    }),
+    react(),
+  ],
   server: {
     open: true,
   },
