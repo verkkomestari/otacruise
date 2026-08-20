4488e96c5f54d0f9be5db74601356a84cfed5f9a
Eemeli Oksanen <eemelioksanen@protonmail.com>
2026-08-20 19:31:51 +0300

    add fish animation

--- src/components/Sea.tsx
+++ src/components/Sea.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { useSpring, animated, easings } from '@react-spring/web'
 import styled, { useTheme } from 'styled-components'
+import pallokala from '../assets/images/pallokala.png'
 
 const SeaContainer = styled.div`
   width: 100%;
   height: 25rem;
-  overflow: hidden;
+  overflow: visible; /* Allows the fish to jump outside the container bounds */
   position: relative;
   background-color: ${({ theme }) => theme.colors.cloud};
 `
@@ -17,6 +18,17 @@ const WAVE_PATH_2 =
   'M 0 40 Q 200 70 400 40 T 800 40 T 1200 40 T 1600 40 T 2000 40 V 400 H 0 Z'
 const WAVE_PATH_3 =
   'M 0 30 Q 120 60 240 30 T 480 30 T 720 30 T 960 30 T 1200 30 T 1440 30 T 1680 30 V 400 H 0 Z'
+
+const randomBetween = (min: number, max: number) =>
+  Math.round(min + Math.random() * (max - min))
+
+interface FishTrajectory {
+  startX: number
+  endX: number
+  startY: number
+  jumpHeight: number
+  direction: number
+}
 
 export const AnimatedSea: React.FC = () => {
   const theme = useTheme()
@@ -72,12 +84,56 @@ export const AnimatedSea: React.FC = () => {
     },
   })
 
+  const fishTrajectory = useRef<FishTrajectory>({
+    startX: 320,
+    endX: 520,
+    startY: 280,
+    jumpHeight: -80,
+    direction: 1,
+  })
+
+  // Jumping fish loop with a new parabolic trajectory on every jump
+  const fishSpring = useSpring({
+    from: { progress: 0 },
+    to: async (next) => {
+      while (true) {
+        const direction = Math.random() < 0.5 ? -1 : 1
+        const jumpDistance = randomBetween(120, 320)
+        const startX =
+          direction === 1 ? randomBetween(120, 620) : randomBetween(380, 880)
+        const endX = startX + direction * jumpDistance
+        const startY = randomBetween(250, 310)
+        const jumpHeight = startY - randomBetween(180, 360)
+
+        fishTrajectory.current = {
+          startX,
+          endX,
+          startY,
+          jumpHeight,
+          direction,
+        }
+
+        // 1. Reset position submerged
+        await next({ progress: 0, config: { duration: 0 } })
+
+        // 2. Pause underwater before jumping
+        await new Promise((res) => setTimeout(res, 2200))
+
+        // 3. Follow one continuous arc from launch to landing
+        await next({
+          progress: 1,
+          config: { duration: 2500, easing: easings.linear },
+        })
+      }
+    },
+  })
+
   return (
     <SeaContainer>
       <svg
         viewBox='0 0 1000 350'
-        preserveAspectRatio='none'
-        style={{ width: '100%', height: '100%' }}
+        preserveAspectRatio='xMidYMid slice'
+        style={{ width: '100%', height: '100%', overflow: 'visible' }}
       >
         <defs>
           {/* Depth gradients */}
@@ -119,6 +175,40 @@ export const AnimatedSea: React.FC = () => {
             style={midWave}
           />
         </g>
+
+        {/* Jumping Fish */}
+        <animated.g
+          style={{
+            transform: fishSpring.progress.to((progress) => {
+              const { startX, endX, startY, jumpHeight, direction } =
+                fishTrajectory.current
+              const arc = 4 * progress * (1 - progress)
+              const x = startX + (endX - startX) * progress
+              const y = startY + (jumpHeight - startY) * arc
+              const rotate = direction * (progress * 110 - 55)
+
+              return `translate(${x}px, ${y}px) rotate(${rotate}deg) scaleX(${direction})`
+            }),
+            transformOrigin: '40px 40px',
+          }}
+        >
+          <svg
+            x='0'
+            y='0'
+            width='80'
+            height='80'
+            viewBox='0 0 80 80'
+            preserveAspectRatio='xMidYMid meet'
+            overflow='visible'
+          >
+            <image
+              href={pallokala}
+              width='80'
+              height='80'
+              preserveAspectRatio='xMidYMid meet'
+            />
+          </svg>
+        </animated.g>
 
         {/* Front Layer */}
         <g transform='translate(0, 190)'>
